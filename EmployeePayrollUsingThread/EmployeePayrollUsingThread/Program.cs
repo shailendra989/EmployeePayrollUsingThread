196ec7f52ff2a1f12fb4ba56eca500072323958a
Shailendra kumar pal <palshailendrakumar123@gmail.com>
2023-02-20 22:18:49 +0530

    first commit

---
+++ EmployeePayrollUsingThread/EmployeePayrollUsingThread/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace EmployeePayrollUsingThread
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Employee Payroll using Threads");
+            string[] words = Demo.CreateWordArray(@"http://www.gutenberg.org/files/54700/54700-0.txt");
+
+            #region ParallelTasks
+            Parallel.Invoke(() =>
+            {
+                Console.WriteLine("Begin first task...");
+                Demo.GetLongestWord(words);
+            },// close 1st action
+            () =>
+            {
+                Console.WriteLine("Begin second task...");
+                Demo.GetMostCommonWords(words);
+            },
+            () =>
+            {
+                Console.WriteLine("Begin third task...");
+                Demo.GetMostCommonWords(words);
+            }// close third action
+            );// close parallel.invoke
+            #endregion
+
+
+        }
+    }
+}
